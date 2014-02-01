24fa7a0f4c427d6a54989bca95975257a9c4cc9f
markus <markus@namaniatsea.org>
2014-02-02 10:07:49 +1100

    Small updates to sl/lib/objects.py.
    See "Notes to Alex" in comments.

--- python/sl/lib/objects.py
+++ python/sl/lib/objects.py
@@ -18,15 +18,30 @@ class Wind(object):
         # we actually want to know where the wind is coming from not where
         # the wind is headed
         self.dir = np.arctan2(-u, -v)
+        # Note to Alex: I moved cardinal directions to the center of the
+        # respective bins, so that you don't get 'NNE' for 1°, or 'SSE' for
+        # 91°, etc.  Also added intermediate directions because I need these
+        # for the wind arrows in the route forecast.  Replaced np.arange by
+        # np.linspace (more robust for ranges bounded by floats/float steps),
+        # and used bisect to find the correct index (your diffs logic was
+        # probably smarter but bisect seemed easier?) 
         # Here we bin according to the standard cardinal directions
-        bins = np.arange(-np.pi, np.pi, step=np.pi / 4)
-        names = ['S', 'SE', 'E', 'NE', 'N', 'NW', 'W', 'SW']
-        diffs = np.mod(self.dir - bins, 2 * np.pi)
-        ind = np.argmin(diffs)
-        if diffs[ind] > np.pi / 4:
-            self.readable = '-'
+        # 'S' is anything before first or after last bin
+        bins = np.linspace(-15 * np.pi/16, 15 * np.pi/16, 16)
+        names = ['S', 'SSW', 'SW', 'WSW', 'W', 'WNW', 'NW', 'NNW', 'N', 'NNE',
+                'NE', 'ENE', 'E', 'ESE', 'SE', 'SSE', 'S']
+        self.readable = names[bisect(bins, self.dir)]
+
+    # Note to Alex: Added nautDir method to get wind direction as 0..2*pi for
+    # display purposes
+    def nautDir(self):
+        """
+        Returns self.dir mapped onto 0..2*pi range
+        """
+        if self.dir >= 0:
+            return self.dir
         else:
-            self.readable = names[ind]
+            return 2 * np.pi + self.dir
 
 
 class LatLon(object):
@@ -36,8 +51,20 @@ class LatLon(object):
     and lon in [0, 360].
     """
     def __init__(self, lat, lon):
+        # Note to Alex: Does lat require modulo? Current assigment doesn't
+        # handle lat==90 correctly
         self.lat = np.mod(lat + 90, 180) - 90
         self.lon = np.mod(lon, 360)
+
+    def nautLatLon(self):
+        """
+        Returns a (lat, lon) tuple with lon in 'nautical' range of -180..180
+        (OpenCPN doesn't handle waypoints with lon > 180 correctly).
+        """
+        if self.lon > 180: 
+            return (self.lat, self.lon - 360)
+        else:
+            return (self.lat, self.lon)
 
     def copy(self):
         return LatLon(self.lat, self.lon)
