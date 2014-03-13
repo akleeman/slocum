6ad8feed457344dd8665c5ff7962547da82887ce
Alex Kleeman <kleeman@climate.com>
2014-03-12 22:59:38 -0700

    Cleanup of slocum.py

--- python/slocum.py
+++ python/slocum.py
@@ -54,7 +54,7 @@ def handle_rtefcst(args):
     else:
         ut = None
     rte = rtefcst.Route(ifh=args.rtefile, inFmt=args.rtefmt, utcDept=ut,
-                        avrgSpeed=args.speed) 
+                        avrgSpeed=args.speed)
     args.rtefile.close()
 
     rf = rtefcst.RouteForecast(rte, fcst)
@@ -79,6 +79,7 @@ def setup_parser_grib(p):
     p.add_argument('--output', type=argparse.FileType('wb'),
                    default=sys.stdout)
 
+
 def setup_parser_email(p):
     """
     Configures the argument subparser for handle_email.  p is the
@@ -89,45 +90,51 @@ def setup_parser_email(p):
                    default=sys.stdout)
     p.add_argument('--ncdf', default=None)
 
+
 def setup_parser_rtefcst(p):
     """
     Configures the argument subparser for handle_rtefcst.  p is the
     ArgumentParser object for the rtefcst subparser.
     """
     p.add_argument('--input', metavar='fcst_file',
-            type=argparse.FileType('rb'), default=sys.stdin, help=
-            'windbreaker forecast file; stdin if not specified')
+                   type=argparse.FileType('rb'), default=sys.stdin,
+                   help='windbreaker forecast file; stdin if not specified')
     p.add_argument('--output', metavar='gpx_file', type=argparse.FileType('w'),
-            help='waypoints with wind data along route; will overwrite if exists; stdout if not specified',
-            default=sys.stdout)
+                   help=('waypoints with wind data along route; will ' +
+                         'overwrite if exists; stdout if not specified'),
+                   default=sys.stdout)
     p.add_argument('--rtefile', metavar='file',
-            type=argparse.FileType('r'), required=True, help=
-            'input file with route definition')
+                   type=argparse.FileType('r'), required=True,
+                   help='input file with route definition')
     p.add_argument('--rtefmt', metavar='fmt', choices=['csv'], default='csv',
-            help='format of route input file; valid formats: csv')
+                   help='format of route input file; valid formats: csv')
     utNowStr = dt.datetime.utcnow().strftime('%Y-%m-%dT%H:%M')
-    p.add_argument('--utcdept', metavar='YYYY-mm-ddTHH:MM', default=utNowStr, 
-            help='utc of departure; default is current utc')
-    p.add_argument('--speed', metavar='SOG', type=float, help=
-            'expected average speed over ground in kn; can be ommitted if rtefile contains speeds for each leg of the route')
-    p.add_argument('--truewind', action='store_true', help=
-            'if specified, output will show true rather than apparent wind at forecast waypoints')
-    p.add_argument('--notimelabel', action='store_true', help=
-            'if specified, time labels will be ommitted from forecast waypoint names')
+    p.add_argument('--utcdept', metavar='YYYY-mm-ddTHH:MM', default=utNowStr,
+                   help='utc of departure; default is current utc')
+    p.add_argument('--speed', metavar='SOG', type=float,
+                   help=('expected average speed over ground in kn; can ' +
+                         'be ommitted if rtefile contains speeds for each ' +
+                         'leg of the route'))
+    p.add_argument('--truewind', action='store_true',
+                   help=('if specified, output will show true rather than ' +
+                         'apparent wind at forecast waypoints'))
+    p.add_argument('--notimelabel', action='store_true',
+                   help=('if specified, time labels will be ommitted from ' +
+                         'forecast waypoint names'))
 
 
 # The _task_handler dictionary maps each 'command' to a (task_handler,
 # parser_setup_handler) tuple.  Subparsers are initialized in __main__  (with
 # the handler function's doc string as help text) and then the appropriate
 # setup handler is called to add the details.
-_task_handler = {'email'   : (handle_email, setup_parser_email),
-                 'grib'    : (handle_grib, setup_parser_grib),
-                 'rtefcst' : (handle_rtefcst, setup_parser_rtefcst)}
+_task_handler = {'email': (handle_email, setup_parser_email),
+                 'grib': (handle_grib, setup_parser_grib),
+                 'rtefcst': (handle_rtefcst, setup_parser_rtefcst)}
 
 if __name__ == "__main__":
 
     parser = argparse.ArgumentParser(description="""
-    Slocum -- A tool for ocean passage planning. 
+    Slocum -- A tool for ocean passage planning.
 
     Joshua Slocum (February 20, 1844 -on or shortly after November 14, 1909)
     was a Canadian-American seaman and adventurer, a noted writer, and the
